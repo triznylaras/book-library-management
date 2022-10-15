55a707894540c35a4f1efb4b3a42c4ade8756fff
triznylaras <triznylaras@gmail.com>
2022-10-15 22:12:22 +0700

    add show, edit, create & set_book in controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: %i[show edit update destroy]
+
   def index
     @books = Book.all
   end
@@ -7,17 +9,31 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-  def add
+  def show
+  end
+
+  def edit
+  end
+
+  def create
     @book = Book.new(book_params)
 
-    if @book.save
-      redirect_to index_path
-    else
-      render :new, status: :unprocessable_entity
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to book_url(@book), notice: "Book was successfully added." }
+        format.json { render :show, status: :created, location: @book }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
 
   def book_params
     params.require(:book).permit(:title, :author, :borrow_time, :return_due_date)
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,3 @@
 class Book < ApplicationRecord
+  validates :title, :author, :borrow_time, :return_due_date, presence: true
 end
